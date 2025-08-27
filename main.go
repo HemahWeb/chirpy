1e6c1289b7b1cb2372c61db21774d907041b55f8
HemahWeb <martin@hemah.de>
2025-08-27 21:42:13 +0200

    fix: move static files into folder 'app'
    Fix was needed to comply with bootdev cli tests. The folder structure had to match exactly.

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir("public")))
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("app"))))
 
 	// Readyness Endpoint
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
