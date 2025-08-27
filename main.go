9b1a6ce5998beab22bed43985922615f2a2d0af8
HemahWeb <martin@hemah.de>
2025-08-27 19:00:38 +0200

    feat: add fileserver and serve index.html

--- main.go
+++ main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
-		w.Header().Set("Content-Type", "text/plain")
-		fmt.Fprintf(w, "Hello, World!")
-	})
+	mux.Handle("/", http.FileServer(http.Dir("public")))
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	server.ListenAndServe()
+	log.Println("Starting server on port 8080")
+	log.Fatal(server.ListenAndServe())
 }
