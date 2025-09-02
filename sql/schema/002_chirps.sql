be1261712dfad7e811373d43084304bcec8a2858
HemahWeb <martin@hemah.de>
2025-09-02 13:39:18 +0200

    fix: correct down migration
    The down migration from the second migration would remove a function that was introduced by the first migration, but the function is still needed by the first migration.

--- sql/schema/002_chirps.sql
+++ sql/schema/002_chirps.sql
@@ -9,17 +9,6 @@ CREATE TABLE chirps (
     ON DELETE CASCADE
 );
 
--- always update updated_at on change
--- +goose StatementBegin
-CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = CURRENT_TIMESTAMP;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
--- +goose StatementEnd
-
 CREATE TRIGGER update_chirps_updated_at 
     BEFORE UPDATE ON chirps 
     FOR EACH ROW 
@@ -27,5 +16,4 @@ CREATE TRIGGER update_chirps_updated_at
 
 -- +goose Down
 DROP TRIGGER IF EXISTS update_chirps_updated_at ON chirps;
-DROP FUNCTION IF EXISTS update_updated_at_column();
 DROP TABLE IF EXISTS chirps;
